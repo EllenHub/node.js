beaa68117189bd803318b85db6959dc4b99b89d4
feden2906 <feden2906@gmail.com>
2021-10-02 12:16:44 +0300

    add some todos

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const path = require('path');
 
 const dirPathBoys = path.join(__dirname, 'lesson1', 'homework', 'boys');
-const dirPathGirls = path.join(__dirname, 'lesson1', 'homework', 'girls')
+const dirPathGirls = path.join(__dirname, 'lesson1', 'homework', 'girls')        // TODO додай ";"
 
 fs.mkdir(dirPathBoys, {recursive: true}, err => {
     if(err) {
@@ -16,35 +16,36 @@ fs.mkdir(dirPathGirls,{recursive: true}, err => {
     if(err) {
         console.log(err);
     }})
-
+                                                                                 // TODO в цьому рядку ентер без потреби, більше ніж один підряд не с тав
 
 function sortFiles(dirPathBoys){
     fs.readdir(dirPathBoys, (err, files) => {
             if(err) {
-                console.log(err)
-            }
+                console.log(err)                                                 // TODO додай ";"
+            }                                                                    // TODO після цього рядка ентер
             files.forEach(file => {
-                const oldFilePath = path.join(dirPathBoys, file)
-                const newFilePath = path.join(dirPathGirls, file)
+                const oldFilePath = path.join(dirPathBoys, file)                 // TODO додай ";"
+                const newFilePath = path.join(dirPathGirls, file)                // TODO додай ";"
                 console.log(file);
 
                 fs.readFile(oldFilePath, (err1, data) => {
                     if(err1) {
-                        console.log(err1)
-                    }
-                    const objData = data.toString()
-                    const objParsed = JSON.parse(objData)
-                    console.log(objParsed);
-                    if(objParsed.gender === 'female') {
+                        console.log(err1)                                        // TODO додай ";"
+                    }                                                            // TODO після цього рядка ентер
+                    const objData = data.toString()                              // TODO додай ";"
+                    const objParsed = JSON.parse(objData)                        // TODO додай ";"
+                    console.log(objParsed);                                      // TODO видаляй зайві логи
+                    if(objParsed.gender === 'female') {                          // TODO 'female' -> динамічним роби
                         fs.rename(oldFilePath, newFilePath, err2 => {
                             if(err2){
-                                console.log(err2)
+                                console.log(err2)                                // TODO додай ";"
                             }
-                        })
+                        })                                                       // TODO додай ";"
                     }
-                })
-            })
- })
+                })                                                               // TODO додай ";"
+            })                                                                   // TODO додай ";"
+ })                                                                              // TODO додай ";"
 }
 
-console.log(sortFiles());
+sortFiles(dirPathBoys);   // TODO передавай ще параменри щоб зробити функцію динамічною. для коректної роботи тобі потіьен "шлях читання", "стать", "шлях куди буде записаний чувак у разі співпадіння гендеру"
+sortFiles(dirPathGirls);  // TODO ...
