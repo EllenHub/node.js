062cd772e69f090106d2dd2ff15bc144e9eb4d83
EllenHub <shirpalalona@gmail.com>
2021-10-06 16:49:18 +0300

    init

--- lesson2/controllers/user.controller.js
+++ lesson2/controllers/user.controller.js
@@ -1,54 +1,41 @@
-const fs = require('fs');
-const path = require('path');
-
-const db = require('../db/users');
-
-const filePath = path.join(__dirname, '../db', 'users.json.txt');
+const  {readUsers,writeData}= require ('../services/user.services')
 
 module.exports = {
     getUsers: (req, res) => {
-        res.json(db)
+
     },
     getUserById: (req, res) => {
         const {user_id} = req.params;
-        const user = db[user_id - 1]
-        if (!user) {
-            res.status(404).json('User is not found')
-            return;
-        }
-        res.json(user)
+        // const user = db[user_id - 1]
+        // if (!user) {
+        //     res.status(404).json('User is not found')
+        //     return;
+        // }
+        res.json('getUser')
     },
-    createUser: (req, res) => {
-        const user = req.body;
+    createUser: async (req, res) => {
+          const user = req.body;
 
-        fs.readFile(filePath, 'utf-8', (err, data) => {
-            if (err) {
-                console.log(err);
-                return;
-            }
-            const objData = JSON.parse(data.toString());
+          const objData = await readUsers();
+console.log(objData);
+          objData.push({...user, id: objData.length + 1});
+            await writeData(objData)
 
-            objData.push({...user, id: objData.length + 1});
-            fs.writeFile(filePath, JSON.stringify(objData), err1 => {
-                if (err1) {
-                    console.log(err1);
-                }
-            });
-            res.json('New user created')
-        });
-    },
+            res.json('New user has been created', objData)
+        },
+
     updateUser: (req, res) => {
         res.json('Update a user')
     },
     deleteUser: (req, res) => {
-        const {user_id} = req.params
-        const user = db[user_id - 1]
-
-        if (user !== -1) {
-            db.splice(user, 1);
-            res.status(204).send();
-        } else {
-            res.status(404).send();
-        }
+        // const {user_id} = req.params
+        // const user = db[user_id - 1]
+        //
+        // if (user !== -1) {
+        //     db.splice(user, 1);
+        //     res.status(204).send();
+        // } else {
+        //     res.status(404).send();
+        // }
     }
 };
